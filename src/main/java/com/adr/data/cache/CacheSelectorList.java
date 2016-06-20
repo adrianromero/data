73375d4d27fe668a3ad772a8d2f8ee94acb974ac
Adrián Romero <adrianromero@users.noreply.github.com>
2016-06-20 13:03:43 +0200

    Cached QueryLink basic implementation

---
+++ src/main/java/com/adr/data/cache/CacheSelectorList.java
@@ -0,0 +1,39 @@
+//     Data Access is a Java library to store data
+//     Copyright (C) 2016 Adrián Romero Corchado.
+//
+//     This file is part of Data Access
+//
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//     
+//         http://www.apache.org/licenses/LICENSE-2.0
+//     
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+package com.adr.data.cache;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ *
+ * @author adrian
+ */
+public class CacheSelectorList implements CacheSelector {
+    
+    private final Set<String> entities;
+    
+    public CacheSelectorList(String... entities) {
+        this.entities = new HashSet<>(Arrays.asList(entities));
+    }
+
+    @Override
+    public boolean cache(String key) {
+        return entities.contains(key);
+    }  
+}
---
+++ src/main/java/com/adr/data/cache/CachedQueryLink.java
@@ -0,0 +1,72 @@
+//     Data Access is a Java library to store data
+//     Copyright (C) 2016 Adrián Romero Corchado.
+//
+//     This file is part of Data Access
+//
+//     Licensed under the Apache License, Version 2.0 (the "License");
+//     you may not use this file except in compliance with the License.
+//     You may obtain a copy of the License at
+//     
+//         http://www.apache.org/licenses/LICENSE-2.0
+//     
+//     Unless required by applicable law or agreed to in writing, software
+//     distributed under the License is distributed on an "AS IS" BASIS,
+//     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//     See the License for the specific language governing permissions and
+//     limitations under the License.
+
+package com.adr.data.cache;
+
+import com.adr.data.DataException;
+import com.adr.data.QueryLink;
+import com.adr.data.QueryOptions;
+import com.adr.data.Record;
+import com.adr.data.utils.EnvelopeResponse;
+import com.adr.data.utils.JSONSerializer;
+import com.adr.data.utils.RequestQuery;
+import com.adr.data.utils.ResponseListRecord;
+import java.util.List;
+
+/**
+ *
+ * @author adrian
+ */
+public class CachedQueryLink implements QueryLink {
+
+    private final QueryLink link;
+    
+    private final CacheProvider provider;
+    private final CacheSelector selector;
+
+    
+    public CachedQueryLink(QueryLink link, CacheProvider provider, CacheSelector selector) {
+        this.link = link;
+        this.provider = provider;
+        this.selector = selector;
+    }
+    
+    public CachedQueryLink(QueryLink link, String... entities) {
+        this(link, new CacheProviderMem(), new CacheSelectorList(entities));
+    }
+
+    @Override
+    public List<Record> query(Record filter, QueryOptions options) throws DataException {
+        String entity = filter.getKey().get("_ENTITY").asString();
+        if (selector.cache(entity)) {           
+            String key = JSONSerializer.INSTANCE.toJSON(new RequestQuery(filter, options));            
+            String cachedresult = provider.get(key); {
+            if (cachedresult == null) {
+                List<Record> l = link.query(filter, options);
+                provider.put(key, JSONSerializer.INSTANCE.toJSON(new ResponseListRecord(l)));
+                return l;
+            } else {
+                EnvelopeResponse response = JSONSerializer.INSTANCE.fromJSONResponse(cachedresult);
+                return response.getAsListRecord();
+            }
+        }
+            
+        } else {
+            return link.query(filter, options);
+        }
+    }
+}
